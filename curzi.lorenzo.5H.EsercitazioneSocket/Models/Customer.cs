7e3278bf6295deb6db1c0e425245fc3875e8f972
loricur321 <loricur@gmail.com>
2021-12-10 16:16:55 +0100

    .

---
+++ curzi.lorenzo.5H.EsercitazioneSocket/Models/Customer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace curzi.lorenzo._5H.EsercitazioneSocket.Models
+{
+    class Customer
+    {
+        public string Customer_id { get; set; }
+
+        public string  Customer_name { get; set; }
+
+        public List<Employee> Employees { get; set; }
+
+        public Customer() 
+        {
+            Employees = new List<Employee>();
+        }
+
+        public Customer (string customer_id, string customer_name, List<Employee> employees)
+        {
+            Customer_id = customer_id;
+            Customer_name = customer_name;
+            Employees = employees;
+        }
+    }
+}
---
+++ curzi.lorenzo.5H.EsercitazioneSocket/Models/Employee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace curzi.lorenzo._5H.EsercitazioneSocket.Models
+{
+    class Employee
+    {
+        public string First_name { get; set; }
+
+        public string Last_name { get; set; }
+
+        public string Phone { get; set; }
+
+        public Employee() { }
+
+        public Employee(string name, string surname, string phone)
+        {
+            First_name = name;
+            Last_name = surname;
+            Phone = phone;
+        }
+    }
+}
